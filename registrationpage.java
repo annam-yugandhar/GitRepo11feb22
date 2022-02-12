0692c2f666b0b8bce27ba8cd6d9e88b699d9b888
annam-yugandhar <yugandharee060@gmail.com>
2022-02-12 21:54:34 +0530

    stash operation

--- registrationpage.java
+++ registrationpage.java
@@ -1,1 +1,7 @@
 this is registration page for customer
+completion of manager work is first proriety
+1 day lines of code
+2 day lines of code
+3 day lines of code 75% task is completed
+4 day lines of code 25% task is completed
+total task is completed in 4days by developer
